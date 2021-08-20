a3400a0800f0f2b63c097d2e9c568ce9d32315c0
nokonoko1203 <healtheworld.1203@gmail.com>
2021-08-20 15:24:28 +0900

    fix next.config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,7 @@
-{
-  "compilerOptions": {
-    "target": "es5",
-    "lib": ["dom", "dom.iterable", "esnext"],
-    "allowJs": false,
-    "skipLibCheck": true,
-    "strict": true,
-    "noImplicitAny": false,
-    "forceConsistentCasingInFileNames": true,
-    "noEmit": true,
-    "esModuleInterop": true,
-    "module": "esnext",
-    "moduleResolution": "node",
-    "resolveJsonModule": true,
-    "isolatedModules": true,
-    "jsx": "preserve",
-    "baseUrl": ".",
-    "paths": {
-      "@/*": ["src/*"]
-    }
-  },
-  "typeRoots": ["types", "node_modules/@types"],
-  "include": ["next-env.d.ts", "**/*.ts", "**/*.tsx"],
-  "exclude": ["node_modules"]
-}
+const isProd = process.env.BUILD_OPTION === 'production';
+
+module.exports = {
+  reactStrictMode: true,
+  distDir: 'build',
+  assetPrefix: isProd ? 'https://example.com/' : '.',
+};
