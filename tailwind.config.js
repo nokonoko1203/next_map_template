361b7056f0afa27eb3e1d465186d393b2f6b2903
nokonoko1203 <healtheworld.1203@gmail.com>
2021-08-20 15:27:04 +0900

    fix tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/layout/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false,
   theme: {
+    fontFamily: {
+      body: [
+        'Avenir',
+        'Helvetica Neue',
+        'Helvetica',
+        'Arial',
+        'Hiragino Sans',
+        'ヒラギノ角ゴシック',
+        'メイリオ',
+        'Meiryo',
+        'YuGothic',
+        'Yu Gothic',
+        'ＭＳ Ｐゴシック',
+        'MS PGothic',
+        'sans-serif',
+      ],
+    },
     extend: {},
   },
   variants: {
