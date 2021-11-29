4532996c35a666c37f14bf3fa25730ea5a0ee452
nokonoko1203 <healtheworld.1203@gmail.com>
2021-11-29 10:43:05 +0900

    format _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,17 +2,17 @@ import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
 
 interface Props {}
 class Document extends NextDocument<Props> {
-    render() {
-        return (
-            <Html lang="ja">
-                <Head></Head>
-                <body>
-                <Main />
-                <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html lang="ja">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default Document;
